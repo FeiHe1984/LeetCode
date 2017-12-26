48324b2e04e92300ff60552ae32074213b8924e2
FeiHe1984 <hpf1111990@gmail.com>
2017-12-27 07:19:18 +0800

    The 15th problem of leetcode

---
+++ 15_PalindromeNumber/palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+	bool isPalindrome(int x) {
+		if (x < 0){
+			return false;
+		}
+
+		int len = 1;
+		while (x / len >= 10){
+			len *= 10;
+		}
+
+		while (x > 0){
+			int left = x / len;
+			int right = x % 10;
+
+			if (left != right){
+				return false;
+			}
+			else{
+				x = (x % len) / 10;
+				len /= 100;
+			}
+		}
+		return true;
+	}
+};
