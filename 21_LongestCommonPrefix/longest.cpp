c9c356ae672b1d62b068e79ee4af7e73ba4ae076
FeiHe1984 <hpf1111990@gmail.com>
2018-01-01 07:35:41 +0800

    The 21st problem of leetcode

---
+++ 21_LongestCommonPrefix/longest.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	string longestCommonPrefix(vector<string> &strs) {
+		int n = strs.size();
+		string res;
+		if (n == 0) return res;
+		for (int pos = 0; pos < strs[0].size(); pos++){
+			for (int k = 1; k < n; k++){
+				if (strs[k].size() == pos || strs[k][pos] != strs[0][pos])
+					return res;
+			}
+			res.push_back(strs[0][pos]);
+		}
+		return res;
+	}
+
+};
