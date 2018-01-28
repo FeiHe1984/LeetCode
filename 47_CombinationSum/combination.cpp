404d4089a510f2e16e4d0331f8b32bb6a8e1f1d7
FeiHe1984 <hpf1111990@gmail.com>
2018-01-29 07:34:44 +0800

    The 47th problem of leetcode

---
+++ 47_CombinationSum/combination.cpp
@@ -0,0 +1,38 @@
+class Solution
+{
+public:
+	vector<vector<int> > combinationSum(vector<int> &candidates, int target)
+	{
+		vector<int> temp; // 用于存放临时组合
+		sort(candidates.begin(), candidates.end());
+		// 结果要求输出的组合不能重复，需要去除candidates中的重复元素
+		candidates.erase(unique(candidates.begin(), candidates.end()), candidates.end());
+		combinationDFS(candidates, temp, 0, target);
+		return result;
+	}
+
+private:
+	vector<vector<int> > result;
+
+	void combinationDFS(vector<int> &candidates, vector<int> &temp, size_t index, int target)
+	{
+		if (target == 0) // 得到满足目标的一组解
+		{
+			result.push_back(temp);
+			return;
+		}
+
+		else
+		{
+			for (size_t i = index; i < candidates.size(); ++i)
+			{
+				if (candidates[i] > target)
+					return;
+
+				temp.push_back(candidates[i]);
+				combinationDFS(candidates, temp, i, target - candidates[i]);
+				temp.pop_back();
+			}
+		}
+	}
+};
