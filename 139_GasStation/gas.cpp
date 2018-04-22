b0ca9b00d88f9cbd51999d7f3f7fc2b8d9c23eb7
FeiHe1984 <hpf1111990@gmail.com>
2018-04-23 06:56:44 +0800

    The 139th problem of leetcode

---
+++ 139_GasStation/gas.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
+		if (gas.empty() || cost.empty() || gas.size() != cost.size()){
+			return -1;
+		}
+
+		int total = 0, sum = 0, start = 0;
+		for (int i = 0; i < gas.size(); i++){
+			total += (gas[i] - cost[i]);
+			if (sum < 0){
+				sum = (gas[i] - cost[i]);
+				start = i;
+			}
+			else{
+				sum += (gas[i] - cost[i]);
+			}
+		}
+		return total < 0 ? -1 : start;
+	}
+};
