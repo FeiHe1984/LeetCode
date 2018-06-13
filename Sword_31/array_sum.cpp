d2ab99e1f351fb3939ba079c32d5cfa8a601ccc1
FeiHe1984 <hpf1111990@gmail.com>
2018-06-14 06:32:36 +0800

    The 31st problem of sowrd interview

---
+++ Sword_31/array_sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	int FindGreatestSumOfSubArray(vector<int> array) {
+		if (array.empty()) return 0;
+		int sum = array[0], tempsum = array[0];
+
+		for (int i = 1; i < array.size(); i++){
+			tempsum = (tempsum < 0) ? array[i] : tempsum + array[i];
+			sum = (tempsum > sum) ? tempsum : sum;
+		}
+		return sum;
+	}
+};
