2657d2856f3e56f4e73293a6ef1b3095c1618d07
FeiHe1984 <hpf1111990@gmail.com>
2018-07-10 07:32:16 +0800

    The 44th problem of sword interview

---
+++ Sword_44/left_rotate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+	string LeftRotateString(string str, int n) {
+		int len = str.length();
+		if (len == 0) return "";
+		n = n % len;
+		str += str;
+		return str.substr(n, len);
+	}
+};
